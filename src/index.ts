29e2763ca0169f679e8879ac1e65b8b0612ee330
Marios <mariosk97@yahoo.gr>
2025-10-10 18:24:05 +0300

    restaurant CRUD added

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,16 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import MyUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+import MyRestaurantRoute from "./routes/MyRestaurantRoute";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=> console.log("Connected to database"))
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME as string, 
+    api_key: process.env.CLOUDINARY_API_KEY as string,
+    api_secret: process.env.CLOUDINARY_API_SECRET as string
+})
 
 const app = express();
 app.use(express.json());
@@ -15,6 +23,7 @@ app.get("/health", async (req: Request, res: Response)=> {
 })
 
 app.use("/api/my/user", MyUserRoute);
+app.use("/api/my/restaurant", MyRestaurantRoute);
 
 app.listen(7000, ()=> {
     console.log("server started on localhost:7000")
--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -17,3 +17,16 @@ export const validateMyUserRequest = [
     body("country").isString().notEmpty().withMessage("Country must be a string"),
     handleValidationErrors,
 ];
+
+export const validateMyRestaurantRequest = [
+    body("restaurantName").notEmpty().withMessage("Restaurant name is required"),
+    body("city").notEmpty().withMessage("City is required"),
+    body("country").notEmpty().withMessage("country is required"),
+    body("deliveryPrice").isFloat({ min: 0 }).withMessage("delivery price must be at least 0"),
+    body("estimatedDeliveryTime").isInt({ min: 0 }).withMessage("estimated delivery time must be at least 0"),
+    body ("cuisines").isArray().withMessage("Cuisines must be an array").not().isEmpty(),
+    body ("menuItems").isArray().withMessage("Menu items must be an array"),
+    body ("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
+    body ("menuItems.*.price").isFloat({ min:0 }).withMessage("Menu item price is required and must be at least 0"),
+    handleValidationErrors,
+];
