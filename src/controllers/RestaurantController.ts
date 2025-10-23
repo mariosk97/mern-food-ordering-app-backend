266c1f017030bfc8514636f2712101dc93db1402
Marios <mariosk97@yahoo.gr>
2025-10-23 10:59:26 +0300

    adde checkout flow

--- src/controllers/RestaurantController.ts
+++ src/controllers/RestaurantController.ts
@@ -1,6 +1,22 @@
 import {Request, Response} from "express"
 import Restaurant from "../models/restaurant";
 
+const getRestaurant = async(req: Request, res: Response) => {
+    try {
+        const restaurantId = req.params.restaurantId;
+        const restaurant = await Restaurant.findById(restaurantId);
+        
+        if(!restaurant) {
+            return res.status(404).json({message: "restaurant not found"})
+        }
+        
+        res.status(200).json(restaurant);
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: "something went wrong"})
+    }    
+}
+
 const searchRestaurants = async (req: Request, res: Response) => {
     try {
         const city = req.params.city!;
@@ -67,5 +83,6 @@ const searchRestaurants = async (req: Request, res: Response) => {
 }
 
 export default {
+    getRestaurant,
     searchRestaurants,
 }
--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -4,13 +4,22 @@ import RestaurantController from "../controllers/RestaurantController";
 
 const router = express.Router()
 
+router.get("/:restaurantId",     
+    param("restaurantId")
+        .isString()
+        .trim()
+        .notEmpty()
+        .withMessage("restaurantId parameter must be a valid string"),
+    RestaurantController.getRestaurant
+);
+
 // api/restaurant/search/london
 router.get("/search/:city", 
     param("city")
-    .isString()
-    .trim()
-    .notEmpty()
-    .withMessage("city parameter must be a valid string"),
+        .isString()
+        .trim()
+        .notEmpty()
+        .withMessage("city parameter must be a valid string"),
     RestaurantController.searchRestaurants
 );    
 
